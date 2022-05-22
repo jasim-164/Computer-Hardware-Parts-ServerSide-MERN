6ef3b8f708099581778e8be11f6c93427a9f7b90
jasim-164 <jasimuddincse1703164@gmail.com>
2022-05-23 01:20:57 +0600

    DB connected

--- index.js
+++ index.js
@@ -4,12 +4,40 @@ const app= express();
 const cors = require('cors');
 //const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port= process.env.PORT ||8000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ce4xb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try{
+        await client.connect();
+        const hardwareCollection = client.db("computer-hardware-parts").collection("hardware-collection");
+    }
+    finally{
+
+    }
+    console.log("db is connected")
+}
+
+
+
+
+
+
+
+
+run().catch(console.dir);
 
 app.get("/",(req, res) =>{
     res.send("data paisi");
