84974ff45b339e28d137c32c5700fd047b59661a
jasim-164 <jasimuddincse1703164@gmail.com>
2022-05-26 23:22:10 +0600

    payment added

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const port= process.env.PORT ||8000;
+
+console.log(stripe);
 
 // middleware
 app.use(cors());
@@ -41,6 +44,8 @@ async function run() {
         const reviewCollection = client.db('computer-hardware-parts').collection('reviews');
         const bookingCollection = client.db('computer-hardware-parts').collection('bookings');
         const paymentCollection = client.db('computer-hardware-parts').collection('payments');
+
+
 
 
         app.get('/products',async(req, res) => {
@@ -130,7 +135,7 @@ async function run() {
             const result = await userCollection.updateOne(filter, updateDoc);
             res.send(result);
           })
-
+          //booking
           app.post('/booking', async (req, res) => {
             const booking = req.body;
             // const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
@@ -144,22 +149,47 @@ async function run() {
             return res.send({ success: true, result });
           });
           app.get('/booking', async (req, res) => {
-            // const booking = {};
-            // // const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
-            // // const exists = await bookingCollection.findOne(query);
-            // // if (exists) {
-            // //   return res.send({ success: false, booking: exists })
-            // // }
-            // const result =  bookingCollection.find(booking);
-            // const bookings = await result.toArray();
-            
-           
-            // res.send(bookings);
+    
             const query = { };
             const bookings = await bookingCollection.find(query).toArray();
             console.log(bookings)
             return res.send(bookings);
           });
+
+          app.get('/booking/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: id};
+            const booking = await bookingCollection.findOne(query);
+            res.send(booking);
+          })
+
+
+          app.post('/create-payment-intent', verifyJWT, async(req, res) =>{
+            const service = req.body;
+            const price = service.price;
+            const amount = price*100;
+            const paymentIntent = await stripe.paymentIntents.create({
+              amount : amount,
+              currency: 'usd',
+              payment_method_types:['card']
+            });
+            res.send({clientSecret: paymentIntent.client_secret})
+          });
+          app.patch('/booking/:id', verifyJWT, async(req, res) =>{
+            const id  = req.params.id;
+            const payment = req.body;
+            const filter = {_id: ObjectId(id)};
+            const updatedDoc = {
+              $set: {
+                paid: true,
+                transactionId: payment.transactionId
+              }
+            }
+      
+            const result = await paymentCollection.insertOne(payment);
+            const updatedBooking = await bookingCollection.updateOne(filter, updatedDoc);
+            res.send(updatedBooking);
+          })
 
     }
     finally{
